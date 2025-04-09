1c672ab9ad9cea394660a4c323e0a92797eabaf5
ibrokhimov.fr <fr.ibrokhimov@gmail.com>
2025-04-09 10:21:31 +0500

    update medication taking schedule with concurrent handling for patient data

--- medtime-move-patient-medication/handler.go
+++ medtime-move-patient-medication/handler.go
@@ -106,3 +106,109 @@ func CreateNewMedication() error {
 
 	return nil
 }
+
+func UpdateTakeTime() error {
+
+	t := time.Now()
+
+	Handler("info", "UpdateTakeTime >>>> ")
+
+	var (
+		getMedicineTUrl  = getListURL + "medicine_taking" + `?data={"frequency":["specific_days","several_times_day"]}`
+		getMedicineTResp = GetListClientApiResponse{}
+	)
+
+	body, err := DoRequest(getMedicineTUrl, "GET", nil)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(body, &getMedicineTResp); err != nil {
+		return err
+	}
+
+	Handler("info", "UpdateTakeTime len >>>> "+fmt.Sprint(len(getMedicineTResp.Data.Data.Response)))
+
+	var wg sync.WaitGroup
+
+	for _, medicine := range getMedicineTResp.Data.Data.Response {
+
+		wg.Add(1)
+
+		go func(medicine map[string]interface{}) {
+			defer wg.Done()
+
+			var (
+				getPatientUrl  = getListURL + "patient_medication" + fmt.Sprintf(`?data={"medicine_taking_id":"%s","is_take":true}`, medicine["guid"])
+				getPatientResp = GetListClientApiResponse{}
+				medicineId     = uuid.NewString()
+				currentAmount  = medicine["current_amount"]
+			)
+
+			body, err := DoRequest(getPatientUrl, "GET", nil)
+			if err != nil {
+				return
+			}
+			if err := json.Unmarshal(body, &getPatientResp); err != nil {
+				return
+			}
+
+			var (
+				deleteMedicineUrl = getSingleURL + "medicine_taking/" + fmt.Sprint(medicine["guid"])
+			)
+
+			_, err = DoRequest(deleteMedicineUrl, "DELETE", Request{Data: map[string]interface{}{}})
+			if err != nil {
+				return
+			}
+
+			medicine["current_amount"] = medicine["current_left"]
+			medicine["guid"] = medicineId
+
+			delete(medicine, "current_left")
+
+			var (
+				createMedicineUrl = getSingleURL + "medicine_taking"
+				createMedicineReq = Request{
+					Data: medicine,
+				}
+			)
+
+			_, err = DoRequest(createMedicineUrl, "POST", createMedicineReq)
+			if err != nil {
+				return
+			}
+
+			for _, patient := range getPatientResp.Data.Data.Response {
+
+				patient["medicine_taking_id"] = medicineId
+
+				var (
+					createPatientUrl = getSingleURL + "patient_medication"
+					createPatientReq = Request{
+						Data: patient,
+					}
+				)
+
+				_, err = DoRequest(createPatientUrl, "POST", createPatientReq)
+				if err != nil {
+					return
+				}
+			}
+
+			createMedicineReq.Data["current_amount"] = currentAmount
+
+			_, err = DoRequest(createMedicineUrl, "PUT", createMedicineReq)
+			if err != nil {
+				return
+			}
+
+		}(medicine)
+
+		wg.Wait()
+
+	}
+
+	Handler("info", "UpdateTakeTime >>>>> "+fmt.Sprint(time.Since(t).String()))
+
+	return nil
+}
